f26f1ee1f54cd55d8fa80807b2185a407622d9e8
yuri de jesus alves <yuri.j.alves@aluno.senai.br>
2025-09-05 11:23:31 -0300

    ADD: dona rosa

---
+++ 03_bot_gemini/class_chat.py
@@ -0,0 +1,59 @@
+import ttkbootstrap as ttk 
+from classe_bot_gemini import Gemini_Bot
+class Janela_chat():
+    def __init__(self):
+        #criando a janela 
+        self.janela= ttk.Window(themename="vapor",
+                       title = "Dona Rosa")
+        self.janela.geometry ("1000x800")
+
+        self.label_titulo = ttk.Label(self.janela,
+                                    text = "OLÁ, SEJA BEM VINDO(A)",
+                                    font = "Stencil",
+                                    foreground = "yellow")
+        self.label_titulo.pack(pady= 10)
+
+        
+        self.label_titulo = ttk.Label(self.janela,
+                                    text = "Venha Aprender a fazer o melhor Brownie do Mundo!",
+                                    font ="Perpetua",
+                                    foreground = "pink")
+        self.label_titulo.pack(pady= 20)
+        
+
+        self.label_titulo = ttk.Label(self.janela,
+                                    text = "Qual é a sua dúvida?",
+                                    font ="Century",
+                                    foreground = "orange")
+        self.label_titulo.pack(pady= 20)
+
+
+        ttk.Button (self.janela,
+                    text="Enviar",
+                    style="danger").pack(pady=20)
+        
+        label_resposta = ttk.Label (self.janela,
+                                    text= "RESPOSTA",
+                                    style= "success")
+        label_resposta.pack(pady=(20,0))
+
+    #crinado o objeto robo(instanciando uma class)
+        self.robo = Gemini_Bot
+
+
+    def responder (self):
+        pergunta = self.entry_pergunta.get()
+        self.robo.enviar_mensagem (pergunta)
+
+    
+    
+    def run (self):
+        self.janela.mainloop()
+
+ 
+
+if __name__ =="__main__":
+    chat = Janela_chat()
+    chat.run()
+
+    
--- 03_bot_gemini/classe_bot_gemini.py
+++ 03_bot_gemini/classe_bot_gemini.py
@@ -1,23 +1,36 @@
 import google.generativeai as genai
 
-class Boot_gemini:
-    """Cria um robo especialista em fazer brownie"""
-
+class Gemini_Bot:
+    """Classe responsável por gerenciar o modelo do Gemini."""
+    
     def __init__(self):
-       genai.configure(api_ky="AIzaSyAwxjTXssSQ9gLXB-Q9QlATJYTk78bpE0g") 
-    
-    
-
-     
+        genai.configure(api_key="AIzaSyAwxjTXssSQ9gLXB-Q9QlATJYTk78bpE0g")
+        
+        instrucao_sistema = """
+            Você é um especialista em confeitaria, principalmemte em brownie com 15 anos de experiência.
+            Seu nome é Dona Rosa. Você deve responder a todas as perguntas de forma 
+            profissional, detalhada e focada exclusivamente no mundo em brwnie. 
+            Se o usuário perguntar sobre outro assunto, gentilmente redirecione a conversa 
+            de volta para brownie, afirmando que seu conhecimento é especializado.
+            """
+        
+        self.model = genai.GenerativeModel(
+            model_name='gemini-1.5-flash',
+            system_instruction=instrucao_sistema
+        )
+        self.chat = self.model.start_chat()
 
     def enviar_mensagem(self, mensagem: str) -> str:
         """Envia mensagem para o modelo e retorna a resposta."""
         response = self.chat.send_message(mensagem)
         return response.text
+    
 
+    #este is so sera execultado se eu rodar o arquivo diretamente 
+    #caso eu importe essa parte não será execultada
+    #podemos utilizar isso para testes 
 
-    def responder (self,pergunta:str) -> str:
-        """Funçao para responder as perguntas enviadas como parametro"""
-
-        response = self.chat.send_message(pergunta)
-        return response.text
+if __name__ =="__main__":
+    robo = Gemini_Bot()
+    resposta = robo.enviar_mensagem("Qual é a flor mais cheirosa do mundo?")
+    print(resposta)
