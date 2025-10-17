4b4ef906d8dd995f03132bc01ba8cc1fb361d517
Helena Rosa <helena.rosa@aluno.senai.br>
2025-10-17 14:57:47 -0300

    ADD; cadastro tkinter

--- 05_lista_tarefas/app.py
+++ 05_lista_tarefas/app.py
@@ -2,3 +2,7 @@ from class_pagina import Logar
 
 janela_principal = Logar()
 janela_principal.run()
+
+
+
+
--- 05_lista_tarefas/cadastro.py
+++ 05_lista_tarefas/cadastro.py
@@ -1,9 +1,12 @@
 import ttkbootstrap as ttk
+import sqlite3
 
 
 class Janela_cadastro():
 
     def _init_(self, janela_pai):
+
+
 
         #Criando a janela filha
         self.janela_cadastro = ttk.Toplevel(janela_pai)
@@ -43,14 +46,73 @@ class Janela_cadastro():
         caixinha_senha = ttk.Entry(self.janela_cadastro)
         caixinha_senha.pack()
 
+        #botão
+        ttk.Button(self.janela_cadastro, text= "Cadastar").pack()
+
+        def criar_tabela_usuario():
+            #Conectando ao banco de dados 
+            conexao= sqlite3.connect ("./bd_lista_tarefas.sqlite")
+
+        #criar cursor
+        cursor = conexao.cursor()
+
+        #Execultar o comando
+        cursor.execute("""
+                    CREATE TABLE usuario (
+                        nome VARCHAR(80),
+                        usuario VARCHAR (20),
+                        senha VARCHAR(20)
+                       );
+                    """)
+        
+        #comito a transação
+        conexao.commit()
+
+        #encerro a conexao
+        conexao.close()
+
+    def inserir_usuario(self):
+        
+        #crir conexao
+        conexao = sqlite3.connect ("./bd_lista_tarefa.sqlite")
 
 
-        def run (self):
-            self.janela_cadastro.mainloop()
+    #criar cursor
+    cursor = conexao.cursor()
+    
+    nome =  self.caixinha_nome.get()
+    usuario = self.caixinha_usuario.get()
+    senha = self.caixinha_senha.get()
+    
 
 
-    if_name_ == "_main_":
+        #execultar
+    cursor.execute("""
+                      INSERT INTO usuario
+                        (nome,
+                        usuario,
+                        enha)
+                    VALUES
+                        (?,
+                         ?,
+                         ?);
+                        """,
+                        [nome, 
+                         usuario, 
+                         senha]
+    )
 
 
-    janela_cadastro = Janaela_cadastro(ttk.Window())
+
+
+
+    def run (self):
+        self.janela_cadastro.mainloop()
+
+    
+    
+if __name__ == "__main__":
+
+
+    janela_cadastro = Janela_cadastro(ttk.Window())
     janela_cadastro.run()
